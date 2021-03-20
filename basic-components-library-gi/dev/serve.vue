044686890598bfeb12b7c9e62f8ecfcee06116d6
Sol <solitude.eb@gmail.com>
2021-03-20 15:51:36 +0200

    rollup v1.0.0

---
+++ basic-components-library-gi/dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { BasicComponentsLibraryGiSample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  BasicComponentsLibraryGiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <basic-components-library-gi-sample />
+  </div>
+</template>
